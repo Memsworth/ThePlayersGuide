6314f37b023f45a9480f67f8e202c9e5d7d8956b
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2022-09-20 17:49:44 +0900

    Looping section done

---
+++ Looping/MagicCannon.cs
@@ -0,0 +1,32 @@
+﻿namespace Looping;
+
+public class MagicCannon
+{
+    public static void RunProgram()
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            if (i % 15 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"{i}: Electric & Fire");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{i}: Fire");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{i}: Electric");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"{i}: Normal");
+            }
+
+        }
+    }
+}
---
+++ Looping/Prototype.cs
@@ -0,0 +1,42 @@
+﻿namespace Looping;
+
+public class Prototype
+{
+    public static void RunProgram()
+    {
+        int targetNumber;
+        int hunterNumber;
+
+        do
+        {
+            Console.Write($"user 1, enter a number between 0 to 100:   ");
+            targetNumber = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException());
+        } while (targetNumber is < 0 or > 100);
+
+        Console.WriteLine($"user 2, guess a number");
+
+        do
+        {
+            Console.Write("What is your next guess? ");
+            hunterNumber = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException());
+
+            if (hunterNumber > targetNumber)
+            {
+                Console.WriteLine($"{hunterNumber} is too high");
+            }
+
+            else if (hunterNumber < targetNumber)
+            { 
+                Console.WriteLine($"{hunterNumber} is too low");
+            }
+                
+        } while (hunterNumber != targetNumber);
+
+
+        if (hunterNumber == targetNumber)
+        {
+            Console.WriteLine("You guessed right");
+        }
+    }
+    
+}
