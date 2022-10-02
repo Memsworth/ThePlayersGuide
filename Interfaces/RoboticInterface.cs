ba9b55749c78b0250736b008dbf9d3499aefe3ea
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2022-10-02 21:34:37 +0900

    Interface Update

---
+++ Interfaces/RoboticInterface.cs
@@ -0,0 +1,111 @@
+﻿namespace Interfaces;
+
+public class RobotTest
+{
+    public static void RunProgram()
+    {
+        Robot robot = new Robot();
+
+        for (int i = 0; i < robot.Commands.Length; i++)
+        {
+            string? input = Console.ReadLine();
+            IRobotCommands newCommand = input switch
+            {
+                "on" => new OnCommand(),
+                "off" => new OffCommand(),
+                "north" => new NorthCommand(),
+                "south" => new SouthCommand(),
+                "east" => new EastCommand(),
+                "west" => new WestCommand(),
+            };
+
+            robot.Commands[i] = newCommand;
+        }
+
+        Console.WriteLine();
+        
+        robot.Run();
+    }
+}
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public IRobotCommands?[] Commands { get; } = new IRobotCommands?[3];
+
+    public void Run()
+    {
+        foreach (IRobotCommands? commands in Commands)
+        {
+            commands.Run(this);
+            Console.WriteLine($"[{X}  {Y}   {IsPowered}]");
+        }
+    }
+}
+public interface IRobotCommands
+{
+    void Run(Robot robot);
+}
+
+public class OnCommand : IRobotCommands
+{
+    public  void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : IRobotCommands
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : IRobotCommands
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y += 1;
+        }
+    }
+}
+
+public class SouthCommand : IRobotCommands
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y -= 1;
+        }
+    }
+}
+
+public class WestCommand : IRobotCommands
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X -= 1;
+        }
+    }
+
+}
+
+public class EastCommand : IRobotCommands
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X += 1;
+        }
+    }
+}
---
+++ Structs/Program.cs
@@ -0,0 +1,40 @@
+﻿
+structTests.RunProgram();
+
+public static class structTests
+{
+    public static void RunProgram()
+    {
+        var a = new Coordinate(5,4);
+        var b = new Coordinate(4, 4);
+        var c = new Coordinate(4, 3);
+        
+        Console.WriteLine(Coordinate.IsAdjacent(a, b)); // Should be True
+        Console.WriteLine(Coordinate.IsAdjacent(b, c)); // Should be True
+        Console.WriteLine(Coordinate.IsAdjacent(a, c)); // Should be True
+    }
+
+}
+
+public struct Coordinate
+{
+    private int Row { get; }
+    private int Column { get; }
+
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public static bool IsAdjacent(Coordinate a, Coordinate b)
+    {
+        int changeRow = a.Row - b.Row;
+        int changeCol = a.Column - b.Column;
+
+        if (Math.Abs(changeRow) <= 1 && changeCol == 0) return true;
+        if (Math.Abs(changeCol) <= 1 && changeRow == 0) return true;
+
+        return false;
+    }
+}
