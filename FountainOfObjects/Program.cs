427fff28bb4a5dcb08e0adb9e2bd1283b0dbc96a
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2022-10-10 11:23:41 +0900

    Classes defined in fountainOfObjects

--- FountainOfObjects/Program.cs
+++ FountainOfObjects/Program.cs
@@ -1,22 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-
-
-public class Map
+﻿public class Map
 {
     public RoomType[,] GameMap { get; private set; }
 
     public Map(uint boardDimensions)
     {
         GameMap = new RoomType[boardDimensions, boardDimensions];
-        int[3,3] = 
     }
+}
+
+public class Location
+{
+    public int Row { get; }
+    public int Col { get; }
+    
+}
+
+public class Fountain
+{
+    public bool FountainCondition { get; private set; }
+
+    public Fountain()
+    {
+        FountainCondition = false;
+    }
+}
+
+public class Player : ISense
+{
+    public string PlayerName { get; }
+    public Player(string playerName)
+    {
+        PlayerName = playerName;
+    }
+    public Direction GetPlayerCommand()
+    {
+        Direction options = Console.ReadLine() switch
+        {
+            "1" => Direction.North,
+            "2" => Direction.East,
+            "3" => Direction.South,
+            "move west" => Direction.West,
+        };
+
+        return options;
+    }
+
+    public RoomSense GetRoomSense()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+public interface ISense
+{
+    public RoomSense GetRoomSense();
 }
 
 public enum RoomType
 {
+    Empty
+};
 
+public enum RoomSense
+{
 };
 public enum Direction
 {
